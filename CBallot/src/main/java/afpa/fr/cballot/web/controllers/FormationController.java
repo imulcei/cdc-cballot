a2da85e164bac18454f03b905aba4195be6b20cd
ByessLB <maui.le.boulicaut@gmail.com>
2025-08-20 12:08:12 +0200

    update FormationController

--- CBallot/src/main/java/afpa/fr/cballot/web/controllers/FormationController.java
+++ CBallot/src/main/java/afpa/fr/cballot/web/controllers/FormationController.java
@@ -20,7 +20,6 @@ import afpa.fr.cballot.services.TeacherService;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletResponse;
 
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
